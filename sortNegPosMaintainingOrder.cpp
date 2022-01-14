a38822bdbbdff22e7118dc2a25ea032ff1e04cd9
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2022-01-14 18:35:28 +0530

    Add files via upload

---
+++ sortNegPosMaintainingOrder.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void sortNeg(int arr[],int size)
+{
+	int p=0,n=1;//p is position of positive and n is for negative
+	
+	while(n<size)
+	{
+		if(arr[n]<0)
+			break;
+		else
+			n++;
+	}
+	while(p<size)
+	{
+		if(arr[p]>=0)
+		{
+			break;
+		}
+		else
+			p++;
+	}
+	while(p<size and n<size)
+	{
+		if(arr[p]>=0)
+		{
+			if(arr[n]<0)
+			{
+				swap(arr[p],arr[n]);
+				p++;
+				n++;
+			}
+			else
+			{
+				n++;
+			}
+		}
+		else
+		{
+			p++;
+		}
+	}
+}
+
+int main()
+{
+	int a[]={-12, 11, -13, -5, 6, -7, 5, -3, -6};
+	sortNeg(a,9);
+	
+	for(int i=0;i<9;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
